b477e8ef4dee33d1b7674d4382262d1a9f48441b
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2024-08-03 22:57:22 -0400

    log scale

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib import ticker
 import pandas as pd
 
 plt.rcParams["figure.figsize"] = [20, 10]
@@ -16,9 +17,21 @@ if 'error/timedout' in dataFrame.values :
 # graph stuff
 
 plt.scatter(dataFrame.Time, dataFrame.Ping,s=3, alpha=0.5,c=dataFrame.Ping, cmap='plasma') # plot
+
+ax = plt.gca();
+
+plt.gca().set_yscale('log')
+
 xticks = plt.gca().get_xticks()
 plt.gca().set_xticks(xticks[::len(xticks) // 10]) # set new tick positions
 plt.gca().tick_params(axis='x', rotation=30) # set tick rotation
+
+# Log scale yaxis tick formatting: https://stackoverflow.com/a/61352317
+ax.yaxis.set_major_locator(ticker.MultipleLocator(100))  # major y tick positions every 100
+
+ax.yaxis.set_minor_locator(ticker.NullLocator())  # no minor ticks
+ax.yaxis.set_major_formatter(ticker.ScalarFormatter())  # set regular formatting
+
 
 
 plt.gcf().autofmt_xdate() # format the dates
